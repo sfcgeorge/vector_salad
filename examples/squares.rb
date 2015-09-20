4b542681127abee09ecde05caeee95f04c4d3d1f
Simon George <simon@sfcgeorge.co.uk>
2015-09-20 22:07:42 +0100

    Update docs.

--- examples/squares.rb
+++ examples/squares.rb
@@ -2,13 +2,13 @@
 square(100)
 
 # Square at position
-square(50, at: [200,100])
+square(50)[200, 100]
 
 # Square with options
-square(30, at: [60,140], fill: 'cyan', stroke: 'magenta', stroke_width: 3).flip_y
+square(30, fill: "cyan", stroke: "magenta", stroke_width: 3)[-60, 140].flip_y
 
 # Square with N node position
-square(40, at: [150, 200], fill: 'magenta').flip(:x)
+square(40, fill: "magenta")[150, -200].flip(:x)
 
 # Square converted to a path and manually added to canvas
-canvas << Square.new(30, at: [250, 130], fill: 'lightblue').to_path
+canvas << Square.new(30, fill: "lightblue")[250, 130].to_path
