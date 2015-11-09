b1931d4c6acda787792a1f53a865c3f722e2a18c
Simon George <simon@sfcgeorge.co.uk>
2015-11-09 01:46:09 +0000

    Improve pal example

--- examples/pall.rb
+++ examples/pall.rb
@@ -1,10 +1,20 @@
-r = 80
-pentagon(r, fill: "green", stroke: "black", stroke_width: 3)[r, r]
+size = 300 # background size
+r = size * 0.4 # body radius
+body_style = { fill: "green", stroke: "black", stroke_width: 3 }
+
+square(size, fill: "skyblue") # background sky
 
 custom(:eye) do |height, **options|
-  oval(height / 2, height, fill: "white")
-  oval(height / 4, height / 2)[0, height * 0.3]
+  oval(height / 2, height, fill: "white") # white
+  oval(height / 4, height / 2)[height * 0.1, height * 0.3] # pupil
 end
 
-eye(r / 2).move(r * 0.7, r)
-eye(r / 2).move(r * 1.3, r)
+move(size / 2, size / 2) do
+  pentagon(r, body_style)
+
+  eye(r / 2).move(-r * 0.3, 0)
+  eye(r / 2).move(r * 0.3, 0)
+
+  circle(r / 4, body_style)[-r * 0.6, r * 0.8] # left hand
+  circle(r / 4, body_style)[r * 0.6, r * 0.8] # right hand
+end
